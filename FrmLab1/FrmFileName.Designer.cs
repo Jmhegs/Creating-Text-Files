7e7961a626f319a321528f914b654a67496e3e7e
Morales <116256085+Jmhegs@users.noreply.github.com>
2022-11-25 16:31:39 +0800

    Add files via upload

---
+++ FrmLab1/FrmFileName.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FrmLab1
+{
+    partial class FrmFileName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textFileName = new System.Windows.Forms.TextBox();
+            this.btnOkay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter FileName";
+            // 
+            // textFileName
+            // 
+            this.textFileName.Location = new System.Drawing.Point(14, 60);
+            this.textFileName.Multiline = true;
+            this.textFileName.Name = "textFileName";
+            this.textFileName.Size = new System.Drawing.Size(320, 36);
+            this.textFileName.TabIndex = 1;
+            // 
+            // btnOkay
+            // 
+            this.btnOkay.Location = new System.Drawing.Point(124, 118);
+            this.btnOkay.Name = "btnOkay";
+            this.btnOkay.Size = new System.Drawing.Size(115, 29);
+            this.btnOkay.TabIndex = 2;
+            this.btnOkay.Text = "Okay";
+            this.btnOkay.UseVisualStyleBackColor = true;
+            this.btnOkay.Click += new System.EventHandler(this.btnOkay_Click);
+            // 
+            // FrmFileName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 172);
+            this.Controls.Add(this.btnOkay);
+            this.Controls.Add(this.textFileName);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmFileName";
+            this.Text = "FrmFileName";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textFileName;
+        private System.Windows.Forms.Button btnOkay;
+    }
+}
---
+++ FrmLab1/FrmRegistration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace FrmLab1
+{
+    public partial class FrmRegistration : Form
+    {
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmRegistration_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string getInput2 = textBox1.Text;
+            string getInput3 = textBox2.Text;
+            string getInput4 = textBox3.Text;
+            string getInput5 = dateTimePicker1.Text;
+            string getInput6 = comboBox1.Text;
+            string getInput7 = textBox4.Text;
+            string getInput8 = textBox5.Text;
+            string getInput9 = comboBox2.Text;
+            string getInput10 = textBox6.Text;
+            string docPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath,
+            getInput2 + ".txt")))
+            {
+                outputFile.WriteLine("Student No:" + getInput2);
+                outputFile.WriteLine("Last Name:" + getInput3);
+                outputFile.WriteLine("Age:" + getInput4);
+                outputFile.WriteLine("Birthday:" + getInput5);
+                outputFile.WriteLine("Program:" + getInput6);
+                outputFile.WriteLine("First Name:" + getInput7);
+                outputFile.WriteLine("MI:" + getInput8);
+                outputFile.WriteLine("Gender:" + getInput9);
+                outputFile.WriteLine("Contact No:" + getInput10);
+
+            }
+
+        }
+    }
+}
